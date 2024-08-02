1d53f7901cd9e4cf89319620b21e21ad1459a993
anshika0704 <anshikachoudhary0704@gmail.com>
2024-08-02 17:52:52 +0530

    Add files via upload
    Initial commit of background color changer project.

---
+++ script.js
@@ -0,0 +1,369 @@
+const button = document.querySelectorAll('.box')
+const body = document.querySelector("body")
+
+button.forEach(function(box){
+    box.addEventListener('click', function(color){
+        if(color.target.id === 'box1'){
+            body.style.backgroundColor = "#ffffff"
+        }
+        if(color.target.id === 'box2'){
+            body.style.backgroundColor = "#f3f1f1"
+        }
+        if(color.target.id === 'box3'){
+            body.style.backgroundColor = "#eceaea"
+        }
+        if(color.target.id === 'box4'){
+            body.style.backgroundColor = "#dad5d5"
+        }
+        if(color.target.id === 'box5'){
+            body.style.backgroundColor ="#c5c2c2"
+        }
+        if(color.target.id === 'box6'){
+          body.style.backgroundColor = "#aca9a9"
+      }
+      if(color.target.id === 'box7'){
+          body.style.backgroundColor = "#a19e9e"
+      }
+      if(color.target.id === 'box8'){
+          body.style.backgroundColor = "#888686"
+      }
+      if(color.target.id === 'box9'){
+          body.style.backgroundColor = "#666464"
+      }
+      if(color.target.id === 'box10'){
+          body.style.backgroundColor ="#474646"
+      }
+      if(color.target.id === 'box11'){
+        body.style.backgroundColor = "#2b2a2a"
+    }
+    if(color.target.id === 'box12'){
+        body.style.backgroundColor ="#0a0a0a"
+    }
+    if(color.target.id === 'box13'){
+      body.style.backgroundColor = "#04202b"
+  }
+  if(color.target.id === 'box14'){
+      body.style.backgroundColor = "#100236f3"
+  }
+  if(color.target.id === 'box15'){
+      body.style.backgroundColor = "#240331da"
+  }
+  if(color.target.id === 'box16'){
+      body.style.backgroundColor = "#380238c4"
+  }
+  if(color.target.id === 'box17'){
+      body.style.backgroundColor ="#6b0443b2"
+  }
+  if(color.target.id === 'box18'){
+    body.style.backgroundColor = "#580808c7"
+}
+if(color.target.id === 'box19'){
+    body.style.backgroundColor = "#522207"
+}
+if(color.target.id === 'box20'){
+    body.style.backgroundColor = "#523f0b"
+}
+if(color.target.id === 'box21'){
+    body.style.backgroundColor = "#5c4806"
+}
+if(color.target.id === 'box22'){
+    body.style.backgroundColor ="#555006"
+}
+if(color.target.id === 'box23'){
+  body.style.backgroundColor = "#3d4704"
+}
+if(color.target.id === 'box24'){
+  body.style.backgroundColor ="#1d3804"
+}
+if(color.target.id === 'box25'){
+  body.style.backgroundColor = "#082f3f"
+}
+if(color.target.id === 'box26'){
+  body.style.backgroundColor = "#041442f3"
+}
+if(color.target.id === 'box27'){
+  body.style.backgroundColor ="#32025ada"
+}
+if(color.target.id === 'box28'){
+body.style.backgroundColor = "#520852c4"
+}
+if(color.target.id === 'box29'){
+body.style.backgroundColor = "#790c4fb2"
+}
+if(color.target.id === 'box30'){
+body.style.backgroundColor = "#6b0c0cc7"
+}
+if(color.target.id === 'box31'){
+body.style.backgroundColor = "#682b07"
+}
+if(color.target.id === 'box32'){
+body.style.backgroundColor ="#685010"
+}
+if(color.target.id === 'box33'){
+body.style.backgroundColor = "#775e0a"
+}
+if(color.target.id === 'box34'){
+body.style.backgroundColor ="#6d660b"
+}
+if(color.target.id === 'box35'){
+  body.style.backgroundColor ="#5b690a"
+}
+if(color.target.id === 'box36'){
+body.style.backgroundColor = "#1f4b06"
+}
+if(color.target.id === 'box37'){
+body.style.backgroundColor = "#154a5f"
+}
+if(color.target.id === 'box38'){
+body.style.backgroundColor = "#0f0d7af3"
+}
+if(color.target.id === 'box39'){
+body.style.backgroundColor = "#3b0483da"
+}
+if(color.target.id === 'box40'){
+body.style.backgroundColor ="#801180c4"
+}
+if(color.target.id === 'box41'){
+body.style.backgroundColor = "#b40f75b2"
+}
+if(color.target.id === 'box42'){
+body.style.backgroundColor ="#bb0f0fc7"
+}
+if(color.target.id === 'box43'){
+body.style.backgroundColor = "#b94d0e"
+}
+if(color.target.id === 'box44'){
+body.style.backgroundColor = "#ac8210"
+}
+if(color.target.id === 'box45'){
+body.style.backgroundColor = "#a07f14"
+}
+if(color.target.id === 'box46'){
+body.style.backgroundColor = "#a09714"
+}
+if(color.target.id === 'box47'){
+body.style.backgroundColor ="#728313"
+}
+if(color.target.id === 'box48'){
+body.style.backgroundColor = "#38660d"
+}
+if(color.target.id === 'box49'){
+body.style.backgroundColor = "#207292"
+}
+if(color.target.id === 'box50'){
+body.style.backgroundColor = "#0a2988f3"
+}
+if(color.target.id === 'box51'){
+body.style.backgroundColor = "#480299da"
+}
+if(color.target.id === 'box52'){
+body.style.backgroundColor ="#a314a3f5"
+}
+if(color.target.id === 'box53'){
+body.style.backgroundColor = "#d10683b2"
+}
+if(color.target.id === 'box54'){
+body.style.backgroundColor ="#f70505f1"
+}
+if(color.target.id === 'box55'){
+body.style.backgroundColor = "#dd5507"
+}
+if(color.target.id === 'box56'){
+body.style.backgroundColor = "#be8d06"
+}
+if(color.target.id === 'box57'){
+body.style.backgroundColor ="#ad9133"
+}
+if(color.target.id === 'box58'){
+body.style.backgroundColor = "#dfd446"
+}
+if(color.target.id === 'box59'){
+body.style.backgroundColor = "#8ca01b"
+}
+if(color.target.id === 'box60'){
+body.style.backgroundColor = "#377c0f"
+}
+if(color.target.id === 'box61'){
+body.style.backgroundColor = "#1c82aa"
+}
+if(color.target.id === 'box62'){
+body.style.backgroundColor ="#160badf3"
+}
+if(color.target.id === 'box63'){
+body.style.backgroundColor = "#640eb4da"
+}
+if(color.target.id === 'box64'){
+body.style.backgroundColor ="#c02dc0f8"
+}
+if(color.target.id === 'box65'){
+  body.style.backgroundColor ="#db1d92f8"
+}
+if(color.target.id === 'box66'){
+body.style.backgroundColor = "#e61e1ef5"
+}
+if(color.target.id === 'box67'){
+body.style.backgroundColor = "#f56f21"
+}
+if(color.target.id === 'box68'){
+body.style.backgroundColor = "#f8ac07"
+}
+if(color.target.id === 'box69'){
+body.style.backgroundColor = "#c09a1d"
+}
+if(color.target.id === 'box70'){
+body.style.backgroundColor ="#f7fa4c"
+}
+if(color.target.id === 'box71'){
+body.style.backgroundColor = "#a8c01e"
+}
+if(color.target.id === 'box72'){
+body.style.backgroundColor ="#589123"
+}
+if(color.target.id === 'box73'){
+body.style.backgroundColor = "#2496c4"
+}
+if(color.target.id === 'box74'){
+body.style.backgroundColor = "#073ad1f3"
+}
+if(color.target.id === 'box75'){
+body.style.backgroundColor = "#7505f5da"
+}
+if(color.target.id === 'box76'){
+body.style.backgroundColor = "#f01ef0d3"
+}
+if(color.target.id === 'box77'){
+body.style.backgroundColor ="#f52aa7fa"
+}
+if(color.target.id === 'box78'){
+body.style.backgroundColor = "#f72c2cc7"
+}
+if(color.target.id === 'box79'){
+body.style.backgroundColor = "#fa8643"
+}
+if(color.target.id === 'box80'){
+body.style.backgroundColor = "#f7be22"
+}
+if(color.target.id === 'box81'){
+body.style.backgroundColor = "#dab53e"
+}
+if(color.target.id === 'box82'){
+body.style.backgroundColor ="#fff676"
+}
+if(color.target.id === 'box83'){
+body.style.backgroundColor = "#c7e037"
+}
+if(color.target.id === 'box84'){
+body.style.backgroundColor ="#67b13d"
+}
+if(color.target.id === 'box85'){
+body.style.backgroundColor = "#41aeda"
+}
+if(color.target.id === 'box86'){
+body.style.backgroundColor = "#6264f0f3"
+}
+if(color.target.id === 'box87'){
+body.style.backgroundColor ="#9543ffda"
+}
+if(color.target.id === 'box88'){
+body.style.backgroundColor = "#f56bf5ef"
+}
+if(color.target.id === 'box89'){
+body.style.backgroundColor = "#f85dbdfb"
+}
+if(color.target.id === 'box90'){
+body.style.backgroundColor = "#fc5c5cd8"
+}
+if(color.target.id === 'box91'){
+body.style.backgroundColor = "#f8975f"
+}
+if(color.target.id === 'box92'){
+body.style.backgroundColor ="#f7c94b"
+}
+if(color.target.id === 'box93'){
+body.style.backgroundColor = "#f1cd55"
+}
+if(color.target.id === 'box94'){
+body.style.backgroundColor ="#fcf69f"
+}
+if(color.target.id === 'box95'){
+body.style.backgroundColor ="#d8f04f"
+}
+if(color.target.id === 'box96'){
+body.style.backgroundColor = "#9cdd60"
+}
+if(color.target.id === 'box97'){
+body.style.backgroundColor = "#89d0ec"
+}
+if(color.target.id === 'box98'){
+body.style.backgroundColor = "#94a8e7f3"
+}
+if(color.target.id === 'box99'){
+body.style.backgroundColor = "#cd92f0da"
+}
+if(color.target.id === 'box100'){
+body.style.backgroundColor ="#f19ff1ef"
+}
+if(color.target.id === 'box101'){
+body.style.backgroundColor = "#f098cefa"
+}
+if(color.target.id === 'box102'){
+body.style.backgroundColor ="#f89a9a"
+}
+if(color.target.id === 'box103'){
+body.style.backgroundColor = "#e9a67f"
+}
+if(color.target.id === 'box104'){
+body.style.backgroundColor = "#ecbf6b"
+}
+if(color.target.id === 'box105'){
+body.style.backgroundColor = "#f3d87f"
+}
+if(color.target.id === 'box106'){
+body.style.backgroundColor = "#a09714"
+}
+if(color.target.id === 'box107'){
+body.style.backgroundColor ="#e7fa79"
+}
+if(color.target.id === 'box108'){
+body.style.backgroundColor = "#bce9a2"
+}
+if(color.target.id === 'box109'){
+body.style.backgroundColor = "#c9e8f5"
+}
+if(color.target.id === 'box110'){
+body.style.backgroundColor = "#e3dafdf3"
+}
+if(color.target.id === 'box111'){
+body.style.backgroundColor = "#e6ccf0da"
+}
+if(color.target.id === 'box112'){
+body.style.backgroundColor ="#fccefcc4"
+}
+if(color.target.id === 'box113'){
+body.style.backgroundColor = "#fdd3edb2"
+}
+if(color.target.id === 'box114'){
+body.style.backgroundColor ="#fdd5d5c7"
+}
+if(color.target.id === 'box115'){
+body.style.backgroundColor = "#fcd7c2"
+}
+if(color.target.id === 'box116'){
+body.style.backgroundColor = "#fce8b2"
+}
+if(color.target.id === 'box117'){
+body.style.backgroundColor ="#fae7a8"
+}
+if(color.target.id === 'box118'){
+body.style.backgroundColor = "#f8f4b4"
+}
+if(color.target.id === 'box119'){
+body.style.backgroundColor = "#eefab1"
+}
+if(color.target.id === 'box120'){
+body.style.backgroundColor = "#dafabc"
+}
+
+    })
+
+})
